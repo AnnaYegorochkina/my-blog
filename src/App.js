b6d1444a0a4c7798cc6b41f8ed57f336305ff857
Hanna_Yahorachkina <hanna_yahorachkina@epam.com>
2022-09-14 13:10:51 +0400

    pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My Awesome Blog</h1>
       <div id="page-body">
-        Welcome to my blog!
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
