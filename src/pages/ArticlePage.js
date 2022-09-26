655a72851bd98b2116e330fbbf3c2a0cab454b4d
Hanna_Yahorachkina <hanna_yahorachkina@epam.com>
2022-09-26 13:02:33 +0400

    axios and comments

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,28 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = () => {
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
+
+    const [ articleInfo, setArticleInfo ] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            // const response = await axios.get(`http://localhost:8000/api/articles/${articleId}`); // after proxy adding no need
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
+        
+        loadArticleInfo();
+    }, []);
 
     if (!article) {
         return <NotFoundPage />
@@ -13,7 +31,9 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
